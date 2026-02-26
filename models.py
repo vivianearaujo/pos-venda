3437d02ac9541791300ab19588dae23afb1d2868
Viviane Santos <32166800+vivianearaujo@users.noreply.github.com>
2026-02-26 19:09:49 -0300

    adição de nova mensagem

--- models.py
+++ models.py
@@ -1,28 +1,26 @@
 import urllib.parse
 
+import urllib.parse
+
 class ServicoPosVenda:
-    def __init__(self, vendedora, cliente, ddd, telefone, data=None, cidade=None):
-        self.vendedora = vendedora
+    def __init__(self, vendedora_planilha, cliente, ddd, telefone, data=None, cidade=None):
+        # Pega apenas o primeiro nome da vendedora da planilha
+        self.vendedora_original = str(vendedora_planilha).strip().split()[0].title() if vendedora_planilha else "Equipe"
         self.cliente = cliente
         self.ddd = ddd
         self.telefone = telefone
+        self.data = data
+        self.cidade = cidade
         
-    def gerar_link_whatsapp(self):
-        # Pega so o primeiro nome da cliente (Ex: Andrea)
+    def gerar_link_whatsapp(self, modelo_texto, nome_remetente):
         nome_cliente = self.cliente.strip().split()[0].title() if self.cliente else "Cliente"
         
         tel_limpo = str(self.telefone).replace("-", "").replace(" ", "").replace(".0", "").strip()
         numero_completo = f"55{self.ddd}{tel_limpo}"
         
-        # MENSAGEM FIXA COM SEU NOME: VIVIANE
-        mensagem = (
-            f"Olá, {nome_cliente}! Tudo bem?\n\n"
-            f"Sou Viviane da My Acessórios do Shopping Tambiá 😄 "
-            f"Estou entrando em contato para saber o que achou das peças e do nosso atendimento. 💓\n\n"
-            f"Ahhhh, também temos atendimento online por WhatsApp com envio por delivery e também no nosso site: "
-            f"https://www.myacessorios.com.br 📲💻\n\n"
-            f"E com o meu cupom (VIVI15) você ainda tem desconto de 15% no nosso site 🥰"
-        )
+        # Substitui as variáveis: {nome_cliente} e {vendedora} (que é quem está enviando agora)
+        mensagem_final = modelo_texto.replace("{nome_cliente}", nome_cliente)
+        mensagem_final = mensagem_final.replace("{vendedora}", nome_remetente)
         
-        msg_codificada = urllib.parse.quote(mensagem)
+        msg_codificada = urllib.parse.quote(mensagem_final)
         return f"https://api.whatsapp.com/send?phone={numero_completo}&text={msg_codificada}"
